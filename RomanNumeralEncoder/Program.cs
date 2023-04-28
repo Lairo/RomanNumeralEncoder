9eeab94aa17b03b489d28b65ba77844423f5c040
chajus1 <chajus1@googlemail.com>
2023-04-28 03:24:23 +0100

    start

--- RomanNumeralEncoder/Program.cs
+++ RomanNumeralEncoder/Program.cs
@@ -10,7 +10,25 @@ namespace RomanNumeralEncoder
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(Solution(1000));
+
+            Console.WriteLine(Solution(1)); // "I"
+            Console.WriteLine(Solution(2)); // "II"
+            Console.WriteLine(Solution(4)); // "IV"
+            Console.WriteLine(Solution(500)); // "D"
+            Console.WriteLine(Solution(1000)); // "M"
+            Console.WriteLine(Solution(1954)); // "MCMLIV"
+            Console.WriteLine(Solution(1990)); // "MCMXC"
+            Console.WriteLine(Solution(2008)); // "MMVIII"
+            Console.WriteLine(Solution(2014)); // "MMXIV"
+
+
+
         }
+        public static string Solution(int n)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
